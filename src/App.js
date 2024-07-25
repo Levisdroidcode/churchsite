07fca1d8c9d3c9caaaba1f4c9d2a8b51a29df586
Levisdroidcode <kimathilevis24@gmail.com>
2024-07-25 16:02:33 +0300

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
-import logo from './logo.svg';
 
 
 function App() {
